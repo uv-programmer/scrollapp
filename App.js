6da6a89a0ef4e56d9013fcf6d302e000d87da8d7
Vishnu-at-196395329624 <Vishnu-at-196395329624>
2020-08-20 12:52:45 +0530

    initial

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const logo =  {
+  uri : "https://reactnative.dev/img/tiny_logo.png",
+  width: 100,
+  height: 100,
+  align:'center'
+};
+const CustomView = () => {
+  return(
+    <ScrollView maximumZoomScale = {2.0} >
+      <View>  
+        <Text style = {{fontSize:85}}>Scroll me please</Text>     
+       <Image source = {logo}/>
+       <Image source = {logo}/>
+       <Image source = {logo}/>
+       <Image source = {logo}/>
+       <Image source = {logo}/>
+       <Image source = {logo}/>
+       <Image source = {logo}/>
+      </View>
+    </ScrollView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default CustomView;
